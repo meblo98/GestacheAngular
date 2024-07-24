ce289b1fe3e738c13fe42cffdbd5543b174f14c7
meblo98 <lomouhamedelbachir@gmail.com>
2024-07-24 21:59:49 +0000

    premier commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  tasks: Task[] = [];
+  newTask: Task = { id: 0, title: '', description: '', completed: false };
+  todayDate: string = '';
+  tasksInfo: string = '';
+
+  ngOnInit(): void {
+    this.getTasksLength();
+  }
+
+
+
+  addTask(): void {
+    if (this.newTask.title && this.newTask.description) {
+      this.newTask.id = new Date().getTime();
+      this.tasks.push({ ...this.newTask });
+      this.newTask = { id: 0, title: '', description: '', completed: false };
+      this.getTasksLength();
+    } else {
+      alert('SVP veillez saisir tous les champs');
+    }
+  }
+
+  deleteTask(index: number): void {
+    this.tasks.splice(index, 1);
+    this.getTasksLength();
+  }
+
+  checked(task: Task): void {
+    task.completed = !task.completed;
+    this.getTasksLength();
+  }
+
+  getTasksLength(): void {
+    const completedTasks = this.tasks.filter(task => task.completed);
+    this.tasksInfo = `${this.tasks.length} Total, ${completedTasks.length} Complete, ${this.tasks.length - completedTasks.length} En attente`;
+  }
+}
